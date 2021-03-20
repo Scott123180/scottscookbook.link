174584bf0978b94c754e0fbba0225b03ee36919d
scott123180 <scott123180@live.com>
2021-03-19 21:14:49 -0400

    adding some reference files

---
+++ scripts/ingredients/IngredientTranslator.py
@@ -0,0 +1,8 @@
+# take in row number aka NDB_No
+
+# ask for file name
+# ask for short name of ingredient
+# ask for food group
+# ask for my plate serving size
+# ask for unit (usually g)
+# ask for serving sizes
