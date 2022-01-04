f9fd74cfc57affb7b8ea185ec3c84b6187ef8bd7
scott123180 <me@scotthansen.io>
2022-01-03 21:45:48 -0500

    fixing recipes

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -13,11 +13,10 @@ import '../styles/global.css';
 const RecipeList = ({ data }) => {
   return (
     <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <Table  aria-label="simple table">
       <TableHead>
         <TableRow>
           <TableCell style={{paddingLeft:'5%'}}>Recipe Name</TableCell>
-          <TableCell>Active Time</TableCell>
           <TableCell>Rating</TableCell>
         </TableRow>
       </TableHead>
@@ -36,28 +35,14 @@ const RecipeList = ({ data }) => {
             <TableCell component="th" scope="row" style={{paddingLeft: '5%'}}>
               {node.frontmatter.title}
             </TableCell>
-            <TableCell><Rating value={node.frontmatter.scottRating} precision={0.5} readOnly/></TableCell>
-            <TableCell>{node.frontmatter.prepTime}</TableCell>
-            <TableCell><Rating value={node.frontmatter.scottRating} precision={0.5} readOnly/></TableCell>
+            <TableCell>
+              <Rating value={node.frontmatter.scottRating} precision={0.5} readOnly/>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
     </Table>
   </TableContainer>
-
-    // <div className="blog-container">
-    //   {data.edges.filter(({node}) => node.frontmatter.topic !== "feature").map(
-    //     ({node}) => (
-    //       <Link key={node.id} to={node.fields.slug} style={{ textDecoration: 'none', color: 'inherit'}}>
-    //         <div className="blog-card">
-    //           <div className="blog-description">
-    //             <h3 className="blog-title">{node.frontmatter.title}</h3>
-    //             <p className="blog-excerpt">{node.excerpt}</p> 
-    //           </div>
-    //         </div>
-    //       </Link>
-    //     ))}
-    // </div>
   );
 }
 
