cd34ea0d095cf0879e01f3f92870ae5e338fff54
scott123180 <me@scotthansen.io>
2022-01-15 14:52:14 -0500

    bean converter page

---
+++ src/pages/BeanConverter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+import SEO from "../components/Seo";
+
+const BeanConverter = ({ data }) => (
+    <Layout>
+        <SEO title="Bean Converter" />
+        <p>can you see me?</p>
+    </Layout>
+);
+
+export default BeanConverter;
