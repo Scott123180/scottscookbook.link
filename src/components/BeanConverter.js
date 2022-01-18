78cbcf89d6c1fd26b42847ca79ced8c38732a7c3
scott123180 <me@scotthansen.io>
2022-01-17 21:43:44 -0500

    working on bean converter

--- src/components/BeanConverter.js
+++ src/components/BeanConverter.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, 
+    TextField, 
+    RadioGroup, 
+    Radio, 
+    FormControlLabel, 
+    Button,
+    Switch } from '@mui/material';
 
 const options = [
     { label: 'Black', id: 1 },
@@ -8,30 +14,87 @@ const options = [
     { label: 'Kidney', id: 4},
     { label: 'Cannelli', id: 5},
     { label: 'Lentil', id: 6},
+    { label: 'Jelly', id: 7},
 ];
+
+const BeanSelector = (
+    <Autocomplete
+    disablePortal
+    id="bean-combo-box"
+    options={options}
+    sx={{ width: 300 }}
+    renderInput={(params) => <TextField {...params} label="Bean / Legume Type" />}
+    />
+);
+
+const BeanStyle = (
+    <div>
+        <p>Bean Style</p>
+        <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="dried"
+            name="radio-buttons-group"
+            row
+        >
+            <FormControlLabel value="dried" control={<Radio />} label="Dried" />
+            <FormControlLabel value="canned" control={<Radio />} label="Canned" />
+            <FormControlLabel value="cooked" control={<Radio />} label="Cooked" />
+        </RadioGroup>
+    </div>
+);
+
+const BeanQuantity = (
+
+    <div>
+        <p>qty</p>
+        <TextField id="standard-basic" label="Quantity" variant="standard" type="number" />
+    </div>
+
+
+);
+
+//generate the options based off of the bean style
+const MeasurementUnit = (
+    <div>
+        <p></p>
+        <p>Unit</p>
+        <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="one"
+            name="radio-buttons-group"
+            row
+        >
+            <FormControlLabel value="one" control={<Radio />} label="Cups" />
+            <FormControlLabel value="two" control={<Radio />} label="Ounce" />
+            <FormControlLabel value="three" control={<Radio />} label="Fluid Ounce" />
+        </RadioGroup>
+    </div>
+);
 
 const BeanConverter = () => {
 
     //extend this so that I can eventually convert it when in a recipe
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
     return (
         <div>
             <p></p>
+            <p>This part of the website is under development. Check back soon!</p>
+            <h1>Bean Converter</h1>
+            <Switch {...label} /> {/* On toggle switch between metric / imperial */}
+
             <p>Recipe Calls for</p>
-            <Autocomplete
-            disablePortal
-            id="bean-combo-box"
-            options={options}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Bean / Legume Type" />}
-            />
 
-            <p>qty</p>
-            <p>unit</p>
-            <p>style --- cooked/canned/dry</p>
+            {BeanSelector}
+            {BeanStyle}
+            {BeanQuantity}
+            {MeasurementUnit}
 
+            <Button variant="contained">Calculate</Button>
+
+            {/* dried, cooked, cans --- volume / weight */}
             <p>results --- converted to other formats</p>
-            <p>include--substitutes</p>
+            <p>include--substitutes (separate tool)</p>
 
         </div>
 
