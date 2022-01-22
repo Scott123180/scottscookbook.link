ddce1391e8e17f9e6d17db97f5d128bc3ab190d5
scott123180 <me@scotthansen.io>
2022-01-22 12:05:30 -0500

    added table

--- src/components/BeanConverter.js
+++ src/components/BeanConverter.js
@@ -8,7 +8,14 @@ import { Autocomplete,
     Button,
     Switch,
     Stack, 
-    Typography} from '@mui/material';
+    Typography,
+    TableContainer, 
+    Paper, 
+    TableHead, 
+    TableCell, 
+    Table, 
+    TableRow, 
+    TableBody} from '@mui/material';
 
 const imperial = "imperial";
 const metric = "metric";
@@ -120,6 +127,55 @@ const MeasurementUnit = (callBackUpdate, measurementUnit, systemOfUnits) => {
     );
 };
 
+const ResultTable = () => {
+
+    function createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+    }
+
+    const rows = [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+      ];
+
+    return (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+    
+
+}
+
 class BeanConverter extends React.Component {
 
     constructor() {
@@ -202,8 +258,9 @@ class BeanConverter extends React.Component {
                 <p>Quantity: {this.state.quantity}</p>
                 <p>MeasurementUnit: {this.state.measurementUnit}</p>
 
-                <p>include--substitutes (separate tool)</p>
+                {ResultTable()}
 
+                <p>include--substitutes (separate tool)</p>
             </div>
         );
     }
