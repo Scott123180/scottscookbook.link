44ca216b25bc1574595e083b6bfff7baa1197e2c
scott123180 <me@scotthansen.io>
2022-03-01 20:22:05 -0500

    changing recipe page to a component

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import IngredientTable from '../components/IngredientTable';
 import Grid from '@mui/material/Grid';
 import Layout from '../components/Layout';
+import { FormGroup, FormControlLabel, Switch } from '@mui/material';
 
 const RecipeLinkElement = ({link}) =>{
 
@@ -14,83 +15,102 @@ const RecipeLinkElement = ({link}) =>{
 
 }
 
-const Post = ({ data }) => {
-  const post = data.markdownRemark;
+class Post extends React.Component{
 
-  const ingredientList = post.frontmatter.ingredients;
-  
-  const mapBySection = new Map();
+  constructor() {
+    super();
 
-  ingredientList.forEach((i) =>{
-    const key = i.section;
-    const value = mapBySection.get(key);
-    if(value === undefined){
-      mapBySection.set(key, [i]);
-    } else {
-      value.push(i);
-    }
-  });
+    this.state = {
+      shoppingModeToggled: false,
+      shoppingProvider: 'Amazon Fresh'
+    };
+  }
 
-  const out = [];
-  mapBySection.forEach((key, value) => {
-    out.push(
-      <Grid container spacing = {2} className="ingredients">
-        <Grid item xs = {1} md={2}/>
-        <Grid item xs = {10} md={8}>
-          <IngredientTable data={mapBySection.get(value)}/>
-        </Grid>
-        <Grid item xs = {1} md={2} />
-      </Grid>
-    )
-  });
+  render(){
+    const data = this.props.data;
+    const post = data.markdownRemark;
 
-  const ingredients = (
-    <div>
-      {out}
-    </div>
-  );
+    const ingredientList = post.frontmatter.ingredients;
+    
+    const mapBySection = new Map();
 
-  const directionList = post.frontmatter.directions;
-  const directions = (
-    <ol>
-      {
-        directionList.map((i) => {
-          return React.createElement(
-            "li",
-            {className:"direction"},
-            i
-          );
-        })
+    ingredientList.forEach((i) =>{
+      const key = i.section;
+      const value = mapBySection.get(key);
+      if(value === undefined){
+        mapBySection.set(key, [i]);
+      } else {
+        value.push(i);
       }
+    });
 
-    </ol>
-  );
+    const out = [];
+    mapBySection.forEach((key, value) => {
+      out.push(
+        <Grid container spacing = {2} className="ingredients">
+          <Grid item xs = {1} md={2}/>
+          <Grid item xs = {10} md={8}>
+            <IngredientTable data={mapBySection.get(value)}/>
+          </Grid>
+          <Grid item xs = {1} md={2} />
+        </Grid>
+      )
+    });
 
-  const originalRecipeLink = post.frontmatter.originalLink;
-  
-  return (
-    <Layout>
-      <div className="post-page-container">
-        <div className="post-page-flex-container">
-          <div className="post-content-container">
-            <h1>{post.frontmatter.title}</h1>
-            <h4 style={{color: 'rgb(165, 164, 164)', fontSize: '0.8em'}}>{post.frontmatter.date} - {post.frontmatter.totalTime}</h4>
-            
-            <h4>Ingredients</h4>
-            {ingredients}
-
-            <h4>Directions</h4>
-            {directions}
-
-            <div dangerouslySetInnerHTML = {{ __html: post.html }}/>
-            <RecipeLinkElement link={post.frontmatter.originalLink} />
-          </div>
-        </div>
-        
+    const ingredients = (
+      <div>
+        {out}
       </div>
-    </Layout>
-  );
-};
+    );
+
+    const directionList = post.frontmatter.directions;
+    const directions = (
+      <ol>
+        {
+          directionList.map((i) => {
+            return React.createElement(
+              "li",
+              {className:"direction"},
+              i
+            );
+          })
+        }
+
+      </ol>
+    );
+
+    const originalRecipeLink = post.frontmatter.originalLink;
+
+    return (
+      <Layout>
+        <div className="post-page-container">
+          <div className="post-page-flex-container">
+            <div className="post-content-container">
+              <h1>{post.frontmatter.title}</h1>
+              <h4 style={{color: 'rgb(165, 164, 164)', fontSize: '0.8em'}}>Published: {post.frontmatter.date} | Total Time: {post.frontmatter.totalTime}</h4>
+              
+              <FormGroup>
+                <FormControlLabel control={<Switch />} label="Shopping Mode" />
+              </FormGroup>
+
+              <h4>Ingredients</h4>
+              {ingredients}
+
+              <h4>Directions</h4>
+              {directions}
+
+              <div dangerouslySetInnerHTML = {{ __html: post.html }}/>
+              <RecipeLinkElement link={post.frontmatter.originalLink} />
+            </div>
+          </div>
+          
+        </div>
+      </Layout>
+    );
+
+  }
+
+} 
 
 export default Post;
 
