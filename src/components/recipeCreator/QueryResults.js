bcc23340ee06aa3e03f412d2475acaa746e4e992
scott123180 <me@scotthansen.io>
2022-05-11 09:30:27 -0400

    adding pagination component

--- src/components/recipeCreator/QueryResults.js
+++ src/components/recipeCreator/QueryResults.js
@@ -6,6 +6,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { TableHead } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 class QueryResults extends React.Component {
 
@@ -20,31 +22,37 @@ class QueryResults extends React.Component {
 
         const foods = this.props.data.foods;
         return (
-            <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                <TableHead>
-                <TableRow>
-                    <TableCell><strong>&nbsp;Description</strong></TableCell>
-                    <TableCell><strong>Source</strong></TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {foods.map((row) => (
-                    <TableRow
-                    key={row.fdcId}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                    <TableCell component="th" scope="row" >
-                        &nbsp;{row.description}
-                    </TableCell>
-                    <TableCell component="th" scope="row" >
-                        &nbsp;{row.dataType}
-                    </TableCell>
+            <div>
+                <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell><strong>&nbsp;Description</strong></TableCell>
+                        <TableCell><strong>Source</strong></TableCell>
                     </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                    {foods.map((row) => (
+                        <TableRow
+                        key={row.fdcId}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row" >
+                            &nbsp;{row.description}
+                        </TableCell>
+                        <TableCell component="th" scope="row" >
+                            &nbsp;{row.dataType}
+                        </TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
+                <Stack>
+                    <Pagination count={this.props.data.pageList.length} color="primary" />
+                </Stack>
+
+            </div>
         );
 
     }
