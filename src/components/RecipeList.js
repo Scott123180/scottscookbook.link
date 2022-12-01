59018ef2bb2c93e43802fabc37cf2189531ffdcc
scott123180 <me@scotthansen.io>
2022-12-01 16:16:07 -0500

    updating gatsbyJs from 4 to 5

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'gatsby-link';
+import {Link} from 'gatsby-link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
